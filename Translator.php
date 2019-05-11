28ab5ac52b3a18494f52b11e89ac6768e266dc0f
Josh Aclan <jaclan@wizmediateam.com>
2019-05-12 01:37:39 +0800

    Add input field

--- Translator.php
+++ Translator.php
@@ -1,6 +1,26 @@
+<!DOCTYPE html>
+<?php
+$t = @$_GET["translate"];
+?>
+<html>
+<head>
+    <title>Simple Translator</title>
+    <script src="main.js"></script>
+    <link rel="stylesheet" href="custom.css">
+</head>
+<body>
+    <div class="container text-center mt-5">
+        <form method="POST">
+          <div class="row form-group mx-sm-3 mb-2">
+            <input type="text" class="col-9 form-control" name="translate" value="<?php echo $t;?>">
+            <input type="submit" class="col-3 btn btn-primary" value="translate">
+        </form>
+    </div>
+</body>
+</html>
 <?php
     // word
-    $input = 'cantik';
+    $input = @$_POST["translate"];
 
     $all_words = array();
     //Get all words in word file 
@@ -21,11 +41,10 @@
     $translate = indo_to_english($word);
     $translated_word = $all_words[$translate];
     // output
-    echo $translated_word;
-
+    echo "Translated word: ".$translated_word;
+    // functions
     function indo_to_english($tword){
         $translated = $tword + 1;
         return $translated;
-}
-
+    }
 ?>
