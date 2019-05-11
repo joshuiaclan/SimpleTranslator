37d4ebfde1cd198e03d17210157b20af5c828de6
Josh Aclan <jaclan@wizmediateam.com>
2019-05-12 01:21:52 +0800

    Indent lines

--- Translator.php
+++ Translator.php
@@ -1,28 +1,31 @@
 <?php
-$input = 'cantik';
+    // word
+    $input = 'cantik';
 
-$all_words = array();
-//Get all words in word file 
-$fn = fopen("Indo-English.txt","r");
-    while(! feof($fn))  {
-	    $result = fgets($fn);
-        // echo $result."</br>";
-        $split_words = preg_split ("[\s]", $result);
-        for ($i = 0; $i < count($split_words); $i++) {
-            // $value = $split_words[$i];
-            array_push($all_words,  $split_words[$i]);
+    $all_words = array();
+    //Get all words in word file 
+    $fn = fopen("Indo-English.txt","r");
+        while(! feof($fn))  {
+            $result = fgets($fn);
+            // echo $result."</br>";
+            $split_words = preg_split ("[\s]", $result);
+            for ($i = 0; $i < count($split_words); $i++) {
+                // $value = $split_words[$i];
+                array_push($all_words,  $split_words[$i]);
+            }
         }
-    }
-fclose($fn);
-$word = array_search("$input", $all_words);
-$translate = indo_to_english($word);
-$translated_word = $all_words[$translate];
+    fclose($fn);
+    // find word
+    $word = array_search("$input", $all_words);
+    // translate
+    $translate = indo_to_english($word);
+    $translated_word = $all_words[$translate];
+    // output
+    echo $translated_word;
 
-echo $translated_word;
-
-function indo_to_english($tword){
-    $translated = $tword + 1;
-    return $translated;
+    function indo_to_english($tword){
+        $translated = $tword + 1;
+        return $translated;
 }
 
 ?>
