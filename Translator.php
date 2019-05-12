d4c8392201381365a296d04b8244fb7b227b82c2
Josh Aclan <jaclan@wizmediateam.com>
2019-05-12 14:18:47 +0800

    Add translate sentence function

--- Translator.php
+++ Translator.php
@@ -19,30 +19,41 @@ $t = @$_GET["translate"];
 </body>
 </html>
 <?php
+    // Arrays
+    $all_words = array();
+    $sentence = array();
     // word
     $input = @$_GET["translate"];
-    
-    $all_words = array();
+    $word_count = str_word_count($input);   
+
     //Get all words in word file 
     $fn = fopen("Indo-English.txt","r");
         while(! feof($fn))  {
             $result = fgets($fn);
-            // echo $result."</br>";
             $split_words = preg_split ("[\s]", $result);
             for ($i = 0; $i < count($split_words); $i++) {
-                // $value = $split_words[$i];
                 array_push($all_words,  $split_words[$i]);
             }
         }
     fclose($fn);
-    // find word
-    $word = array_search("$input", $all_words);
-    if($word == null){
-        echo "$input";
+    if ($word_count > 1){
+        $sentence = preg_split ("[\s]", $input);
+        for ($i = 0; $i < count($sentence); $i++) {
+            $word = array_search($sentence[$i], $all_words);
+            $translate = indo_to_english($word);
+            $translated_word = $all_words[$translate];
+            echo " ".$translated_word;
+        }
     } else {
-        $translate = indo_to_english($word);
-        $translated_word = $all_words[$translate];
-        echo "Translated word: ".$translated_word;
+    // find word
+        $word = array_search("$input", $all_words);
+        if($word == null){
+            echo "$input";
+        } else {
+            $translate = indo_to_english($word);
+            $translated_word = $all_words[$translate];
+            echo "</br>Translated word: ".$translated_word;
+        }
     }
     // functions
     function indo_to_english($tword){
